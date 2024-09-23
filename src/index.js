2c93bc345b418edf98525857cbbd0acfef2755f1
Ryna <katrin_dr@naver.com>
2024-09-23 20:24:57 +0900

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number === 0) {
+        return ones[number];
+    }
+
+    let readableString = "";
+
+    if (Math.floor(number / 100) > 0) {
+        readableString += ones[Math.floor(number / 100)] + " hundred";
+        number %= 100;
+        if (number > 0) {
+            readableString += " ";
+        }
+    }
+
+    if (number >= 20) {
+        readableString += tens[Math.floor(number / 10)];
+        number %= 10;
+        if (number > 0) {
+            readableString += " ";
+        }
+    } else if (number >= 10 && number <= 19) {
+        readableString += teens[number - 10];
+        number = 0;
+    }
+
+    if (number > 0 && number < 10) {
+        readableString += ones[number];
+    }
+
+    return readableString.trim();
+};
